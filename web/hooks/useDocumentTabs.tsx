f07574f0200c19d0441b985bb5e5ecf8101666f6
ibrahim <ibrahimyaacob92@yahoo.com>
2023-12-01 14:54:56 +0800

    fix: enable document tabs to be closed when document itself is deleted

--- web/hooks/useDocumentTabs.tsx
+++ web/hooks/useDocumentTabs.tsx
@@ -137,10 +137,10 @@ const useDocumentTabs = () => {
 		});
 	};
 
-	const closeTab = (source: string) => {
-		const filteredTab = projectDocumentTabsMemory[projectId].filter(
-			tab => tab.source !== source,
-		);
+	const closeTab = (source: string, _projectId?: string) => {
+		const filteredTab = projectDocumentTabsMemory[
+			_projectId || projectId
+		].filter(tab => tab.source !== source);
 
 		const noTabActive = filteredTab.every(tab => tab.active === false);
 		if (noTabActive) {
@@ -151,7 +151,7 @@ const useDocumentTabs = () => {
 
 		setProjectDocumentTabsMemory({
 			...projectDocumentTabsMemory,
-			[projectId]: filteredTab,
+			[_projectId || projectId]: filteredTab,
 		});
 
 		// Remove search cache here
@@ -182,7 +182,7 @@ const useDocumentTabs = () => {
 		});
 	};
 
-	const deleteProject = (projectId: string) => {
+	const deleteProjectFromTabMemory = (projectId: string) => {
 		const { [projectId]: deletedKey, ...rest } = projectDocumentTabsMemory;
 		setProjectDocumentTabsMemory(rest);
 	};
@@ -253,7 +253,7 @@ const useDocumentTabs = () => {
 		renameTab,
 		addProject,
 		handleTabOnSort,
-		deleteProject,
+		deleteProjectFromTabMemory,
 		toggleDocumentByTabIndex,
 		updateLiteratureSearchTab,
 		updateNewChatTab,
