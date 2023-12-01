0bf6f9de0c43a86aea23f30feb5f0b789989e94e
Eimen Hamedat <64800243+eimenhmdt@users.noreply.github.com>
2023-12-01 14:40:41 +0400

    Update ProjectExplorer.tsx

--- web/components/ProjectExplorer.tsx
+++ web/components/ProjectExplorer.tsx
@@ -41,7 +41,7 @@ const ProjectExplorer = memo(() => {
 			)}
 		>
 			<div className="flex items-center justify-between p-3">
-				<p className="text-sm font-medium text-foreground">Projects</p>
+				<p className="text-sm font-semibold text-foreground">Projects</p>
 				<div className="inline-flex items-center gap-0.5">
 					<Popover open={createProjectPopoverOpen}>
 						<Tooltip>
