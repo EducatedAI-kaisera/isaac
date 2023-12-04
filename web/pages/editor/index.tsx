e9735e2e774146b2bf4fd248b88adc8e919d6fe6
Eimen Hamedat <64800243+eimenhmdt@users.noreply.github.com>
2023-12-05 02:29:38 +0400

    add onboarding for first project

--- web/pages/editor/index.tsx
+++ web/pages/editor/index.tsx
@@ -1,4 +1,7 @@
 import EditorLayout from '@components/editor/EditorLayout';
+import { Logomark } from '@components/landing/Logo';
+import { Button } from '@components/ui/button';
+import Ripple from '@components/ui/ripple';
 import {
 	Select,
 	SelectContent,
@@ -14,6 +17,7 @@ import useHandleToastQuery from '@hooks/misc/useHandleToastQuery';
 import {} from '@radix-ui/react-select';
 import { useGetProjects } from '@resources/editor-page';
 import clsx from 'clsx';
+import { FolderPlus } from 'lucide-react';
 import Head from 'next/head';
 import Link from 'next/link';
 import React, { useEffect, useMemo, useState } from 'react';
@@ -39,6 +43,9 @@ const EditorPage = () => {
 	const { data: projects, isLoading: isGetProjectsLoading } =
 		useGetProjects(user);
 	const editorWidth = useUIStore(s => s.editorWidth);
+	const setCreateNewProjectModalOpen = useUIStore(
+		s => s.setCreateProjectPopoverOpen,
+	);
 	useHandleToastQuery();
 
 	const projectGridClasses = useMemo(
@@ -111,27 +118,58 @@ const EditorPage = () => {
 			<EditorHead />
 			<style>{editorPageStyle}</style>
 			<EditorLayout>
-				<div className="w-full">
-					<h1 className="text-center">Projects</h1>
+				{renderedProjects?.length > 2000 ? (
+					<div className="w-full">
+						<h1 className="text-center text-foreground">Projects</h1>
 
-					<Select onValueChange={handleSortChange}>
-						<SelectTrigger className={clsx('w-[240px] mx-auto my-10')}>
-							<SelectValue placeholder="Recently created" />
-						</SelectTrigger>
-						<SelectContent>
-							<SelectGroup>
-								{/* <SelectItem value="lastOpened">Last Opened</SelectItem> */}
-								<SelectItem value="recentlyCreated">
-									Recently created
-								</SelectItem>
-								<SelectItem value="name">Name</SelectItem>
-							</SelectGroup>
-						</SelectContent>
-					</Select>
-					<div className={projectGridClasses}>
-						{isGetProjectsLoading ? <LoadingPlaceholder /> : renderedProjects}
+						<Select onValueChange={handleSortChange}>
+							<SelectTrigger className={clsx('w-[240px] mx-auto my-10')}>
+								<SelectValue placeholder="Recently created" />
+							</SelectTrigger>
+							<SelectContent>
+								<SelectGroup>
+									{/* <SelectItem value="lastOpened">Last Opened</SelectItem> */}
+									<SelectItem value="recentlyCreated">
+										Recently created
+									</SelectItem>
+									<SelectItem value="name">Name</SelectItem>
+								</SelectGroup>
+							</SelectContent>
+						</Select>
+						{isGetProjectsLoading ? (
+							<div className={projectGridClasses}>
+								<LoadingPlaceholder />
+							</div>
+						) : (
+							<div className={projectGridClasses}>{renderedProjects}</div>
+						)}
 					</div>
-				</div>
+				) : (
+					<div className="flex h-screen flex-col items-center mx-auto justify-center text-center">
+						<div className="inline-flex items-center text-center mb-4 fixed top-96 ">
+							<Logomark className="w-12 h-12" />
+							<p className="text-2xl font-bold tracking-tighter text-foreground">
+								Isaac
+							</p>
+						</div>
+
+						<Button
+							size="lg"
+							className="z-10 bg-isaac hover:bg-white hover:text-isaac mt-6"
+							onClick={() => setCreateNewProjectModalOpen(true)}
+						>
+							<FolderPlus className="mr-2 " strokeWidth={1.2} size={20} />
+							Create your first project
+						</Button>
+						<div className="mt-4">
+							<p className="text-sm text-foreground mt-3">
+								A project in Isaac is a workspace for your documents,
+								references, and notes. Get started now!
+							</p>
+						</div>
+						<Ripple />
+					</div>
+				)}
 			</EditorLayout>
 		</>
 	);
