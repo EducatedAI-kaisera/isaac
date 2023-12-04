be81797285c36d13b1af9cc989e5ee93afd370f8
Eimen Hamedat <64800243+eimenhmdt@users.noreply.github.com>
2023-12-05 03:58:58 +0400

    Update ProjectExplorer.tsx

--- web/components/ProjectExplorer.tsx
+++ web/components/ProjectExplorer.tsx
@@ -1,12 +1,6 @@
-import CreateNewProjectInput from '@components/core/CreateNewProjectInput';
 import ProjectGroup from '@components/ProjectGroup';
 import SidebarFooter from '@components/SidebarFooter';
 import { Button } from '@components/ui/button';
-import {
-	Popover,
-	PopoverContent,
-	PopoverTrigger,
-} from '@components/ui/popover';
 import {
 	Tooltip,
 	TooltipContent,
@@ -14,15 +8,12 @@ import {
 } from '@components/ui/tooltip';
 import { useUIStore } from '@context/ui.store';
 import useGetEditorRouter from '@hooks/useGetEditorRouter';
-import { useClickOutside } from '@mantine/hooks';
 import { useTour } from '@reactour/tour';
 import clsx from 'clsx';
 import { FilePlus, FolderPlus } from 'lucide-react';
-import { useRouter } from 'next/router';
 import React, { memo } from 'react';
 
 const ProjectExplorer = memo(() => {
-	const createProjectPopoverOpen = useUIStore(s => s.createProjectPopoverOpen);
 	const setCreateProjectPopoverOpen = useUIStore(
 		s => s.setCreateProjectPopoverOpen,
 	);
@@ -30,9 +21,6 @@ const ProjectExplorer = memo(() => {
 		s => s.setShowCreateDocumentModal,
 	);
 
-	const { push } = useRouter();
-	const handleClickOutside = () => setCreateProjectPopoverOpen(false);
-	const createProjectRef = useClickOutside(handleClickOutside);
 	const { isOpen: tutorialMode, setCurrentStep } = useTour();
 	const { projectId: activeProjectId } = useGetEditorRouter();
 	return (
@@ -45,44 +33,6 @@ const ProjectExplorer = memo(() => {
 			<div className="flex items-center justify-between p-3">
 				<p className="text-sm font-semibold text-foreground">Projects</p>
 				<div className="inline-flex items-center gap-0.5">
-					<Popover open={createProjectPopoverOpen}>
-						<Tooltip>
-							<TooltipTrigger asChild>
-								<PopoverTrigger id="create-project-button" asChild>
-									<Button
-										size="icon"
-										variant="ghost"
-										className="w-6 h-6"
-										onClick={() => {
-											setCreateProjectPopoverOpen(true);
-											tutorialMode && setCurrentStep(prev => prev + 1);
-										}}
-									>
-										<FolderPlus size={16} strokeWidth={1.2} />
-									</Button>
-								</PopoverTrigger>
-							</TooltipTrigger>
-							<TooltipContent side="right">
-								<p>New project </p>
-							</TooltipContent>
-						</Tooltip>
-						<PopoverContent
-							className="p-1"
-							ref={createProjectRef}
-							id="create-project-input"
-						>
-							<CreateNewProjectInput
-								onSuccess={data => {
-									setCreateProjectPopoverOpen(false);
-									if (tutorialMode) {
-										push(`/editor/${data.id}`);
-										setTimeout(() => setCurrentStep(prev => prev + 1), 500);
-									}
-								}}
-							/>
-						</PopoverContent>
-					</Popover>
-					{activeProjectId && (
 					<Tooltip>
 						<TooltipTrigger asChild>
 							<Button
@@ -90,16 +40,36 @@ const ProjectExplorer = memo(() => {
 								variant="ghost"
 								className="w-6 h-6"
 								onClick={() => {
-									setShowCreateDocumentModal(true);
+									setCreateProjectPopoverOpen(true);
+									tutorialMode && setCurrentStep(prev => prev + 1);
 								}}
 							>
-								<FilePlus size={16} strokeWidth={1.2} />
+								<FolderPlus size={16} strokeWidth={1.2} />
 							</Button>
 						</TooltipTrigger>
 						<TooltipContent side="right">
-							<p>New document </p>
+							<p>New project </p>
 						</TooltipContent>
 					</Tooltip>
+
+					{activeProjectId && (
+						<Tooltip>
+							<TooltipTrigger asChild>
+								<Button
+									size="icon"
+									variant="ghost"
+									className="w-6 h-6"
+									onClick={() => {
+										setShowCreateDocumentModal(true);
+									}}
+								>
+									<FilePlus size={16} strokeWidth={1.2} />
+								</Button>
+							</TooltipTrigger>
+							<TooltipContent side="right">
+								<p>New document </p>
+							</TooltipContent>
+						</Tooltip>
 					)}
 				</div>
 			</div>
