5d54e4376f93e307b3636480e8244a0d3be16b29
Eimen Hamedat <64800243+eimenhmdt@users.noreply.github.com>
2023-12-04 22:29:59 +0400

    fix: remove hardcoded host name

--- web/utils/stream_response.ts
+++ web/utils/stream_response.ts
@@ -1,7 +1,8 @@
 import fetch from 'node-fetch';
 
 export async function performCompletion(res, body, inPlace = false) {
-    const completion = await fetch('http://0.0.0.0:5001/api/completion', {
+    const API_HOST = process.env.API_HOST;
+    const completion = await fetch(`${API_HOST}/api/completion`, {
         method: 'POST',
         body: JSON.stringify(body),
         headers: { 'Content-Type': 'application/json', 'x-api-key': 'default-api-route-secret' },
