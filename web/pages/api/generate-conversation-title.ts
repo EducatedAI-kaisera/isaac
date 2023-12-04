f111f9fff974a01539d674a8c175297711e9d55e
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2023-12-04 18:02:05 +0530

    revert non-chat calls

--- web/pages/api/generate-conversation-title.ts
+++ web/pages/api/generate-conversation-title.ts
@@ -1,6 +1,10 @@
 import { NextApiRequest } from 'next';
-import { ChatCompletionRequestMessage } from 'openai';
-import { performCompletion } from '../../utils/stream_response'
+import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+	apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async function (req: NextApiRequest, res) {
@@ -13,17 +17,26 @@ export default async function (req: NextApiRequest, res) {
 		messages: ChatCompletionRequestMessage[];
 	};
 
-	await performCompletion(res, {
-		model: 'gpt-3.5-turbo-instruct',
-		temperature: 0.6,
-		prompt: `
-		From the chat conversation below, generate a short title for it.
-		Response must be just the title of words between 3 to 8 words.
-		The title must be the same language as the language of the conversation.
+	const completion = await openai.createCompletion(
+		{
+			model: 'gpt-3.5-turbo-instruct',
+			temperature: 0.6,
+			prompt: `
+			From the chat conversation below, generate a short title for it.
+			Response must be just the title of words between 3 to 8 words.
+			The title must be the same language as the language of the conversation.
 
-		conversation:
-		${messages.map(i => `${i.role}: ${i.content}.`).join('\n')}
-		`,
-		stream: true,
-	})
+			conversation:
+			${messages.map(i => `${i.role}: ${i.content}.`).join('\n')}
+			`,
+			stream: true,
+		},
+
+		{ responseType: 'stream' },
+	);
+
+	// @ts-expect-error
+	completion.data.on('data', data => {
+		res.write(data.toString());
+	});
 }
