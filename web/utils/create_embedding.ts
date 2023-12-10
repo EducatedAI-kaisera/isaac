5b79fa068853a43d23e996984468796cf6ee4c4f
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2023-12-10 20:45:36 +0530

    Update create_embedding.ts

--- web/utils/create_embedding.ts
+++ web/utils/create_embedding.ts
@@ -1,8 +1,8 @@
 export async function createEmbedding(body) {
-    const embedding = await fetch('http://0.0.0.0:5001/api/embedding', {
+    const embedding = await fetch(`${process.env.NEXT_PUBLIC_API_HOST}/api/embedding`, {
         method: 'POST',
         body: JSON.stringify(body),
-        headers: { 'Content-Type': 'application/json', 'x-api-key': 'default-api-route-secret' },
+        headers: { 'Content-Type': 'application/json', 'x-api-key': process.env.NEXT_PUBLIC_API_ROUTE_SECRET },
     });
     const response = await embedding.json()
     return response
