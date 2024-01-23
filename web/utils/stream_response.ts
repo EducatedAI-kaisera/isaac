b28ac7998a15229b75b09793806e7b535a0f48ee
Eimen Hamedat <64800243+eimenhmdt@users.noreply.github.com>
2024-01-23 02:59:29 +0100

    fix: solve vercel streaming problem

--- web/utils/stream_response.ts
+++ web/utils/stream_response.ts
@@ -1,31 +1,43 @@
 import fetch from 'node-fetch';
 
 export async function performCompletion(res, body, inPlace = false) {
-    const completion = await fetch(`${process.env.NEXT_PUBLIC_API_HOST}/api/completion`, {
-        method: 'POST',
-        body: JSON.stringify(body),
-        headers: { 'Content-Type': 'application/json', 'X-API-KEY': process.env.NEXT_PUBLIC_API_ROUTE_SECRET },
-    });
-    if (body['stream']) {
-        // Send an initial comment to establish the connection
-        res.write(':ok\n\n');
+	const completion = await fetch(
+		`${process.env.NEXT_PUBLIC_API_HOST}/api/completion`,
+		{
+			method: 'POST',
+			body: JSON.stringify(body),
+			headers: {
+				'Content-Type': 'application/json',
+				'X-API-KEY': process.env.NEXT_PUBLIC_API_ROUTE_SECRET,
+			},
+		},
+	);
+	if (body['stream']) {
+		// Send an initial comment to establish the connection
+		res.write(':ok\n\n');
 
-        // Forward the data from the completion request to the client
-        completion.body.on('data', chunk => {
-            res.write(`data: ${chunk.toString('base64')}\n\n`);
-        });
-        // Handle completion request completion
-        completion.body.on('end', () => {
-            // Send a final chunk to the frontend that says [DONE]
-            const doneMessage = Buffer.from('[DONE]').toString('base64');
-            res.write(`data: ${doneMessage}\n\n`);
-            // Close the SSE connection when the completion request is complete
-            res.end();
-        });
-    }
-    else {
-        const response = await completion.text()
-        if (inPlace) return response
-        else res.end(response)
-    }
+		// Forward the data from the completion request to the client
+		completion.body.on('data', chunk => {
+			const encoder = new TextEncoder();
+			const encodedData = encoder.encode(
+				`data: ${chunk.toString('base64')}\n\n`,
+			);
+			res.write(encodedData);
+		});
+		// Handle completion request completion
+		completion.body.on('end', () => {
+			// Send a final chunk to the frontend that says [DONE]
+			const encoder = new TextEncoder();
+			const encodedDoneMessage = encoder.encode(
+				`data: ${Buffer.from('[DONE]').toString('base64')}\n\n`,
+			);
+			res.write(encodedDoneMessage);
+			// Close the SSE connection when the completion request is complete
+			res.end();
+		});
+	} else {
+		const response = await completion.text();
+		if (inPlace) return response;
+		else res.end(response);
+	}
 }
