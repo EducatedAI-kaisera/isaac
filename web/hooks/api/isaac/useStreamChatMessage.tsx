46ef41bbe548a82b9416440246d016022c7a61ee
Eimen Hamedat <64800243+eimenhmdt@users.noreply.github.com>
2024-01-30 23:39:25 +0100

    prevent errors being thrown when free trial limits reached

--- web/hooks/api/isaac/useStreamChatMessage.tsx
+++ web/hooks/api/isaac/useStreamChatMessage.tsx
@@ -6,6 +6,8 @@ import { useQueryClient } from 'react-query';
 import { SSE } from 'sse.js';
 import { ChatContext } from 'types/chat';
 import { base64ToUint8Array } from '@utils/base64ToUint8Array';
+import { freePlanLimits } from 'data/pricingPlans';
+import { ProPlanUpgradeToast, reachedTokenLimitToastStyle } from '@components/toast/ProPlanUpgradToast';
 
 const useStreamChatMessage = () => {
 	const { user } = useUser();
@@ -37,6 +39,17 @@ const useStreamChatMessage = () => {
 		});
 
 		try {
+
+			if (
+				user.is_subscribed === false &&
+				user.daily_free_token === freePlanLimits.dailyFreeToken
+			) {
+				toast.error(<ProPlanUpgradeToast target="AI" />, {
+					style: reachedTokenLimitToastStyle,
+				});
+				return;
+			}
+
 			const source = new SSE('/api/chat', { payload });
 			let cumulativeChunk = '';
 
