79ffaf45d29e4844c8abe618481f8595d9ee40b0
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2024-01-31 14:10:16 +0530

    fix: res

--- web/middleware.js
+++ web/middleware.js
@@ -1,22 +1,17 @@
 import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
 import { NextResponse } from 'next/server';
 
-export async function middleware(request) {
-	// Create a Supabase client configured to use cookies
+export async function middleware(req) {
+	const res = NextResponse.next();
 	const supabase = createMiddlewareClient({ req, res });
 	const { data } = await supabase.auth.getUser();
-	if (!data.user) {
-		return NextResponse.redirect(new URL('/signup', request.url));
-	}
+	if (!data.user) return NextResponse.redirect(new URL('/signup', req.url));
 	const result = await supabase
 		.from('profile')
 		.select('is_subscribed, expiration_date, plan')
 		.eq('id', data.user.id)
 		.single();
-	// If user isn't logged in (no profile data), redirect to signup page
-	if (!result.data) {
-		return NextResponse.redirect(new URL('/signup', request.url));
-	}
+	if (!result.data) return NextResponse.redirect(new URL('/signup', req.url));
 	return NextResponse.next();
 }
 
