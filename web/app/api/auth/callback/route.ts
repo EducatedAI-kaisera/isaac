b6daf235ef2352f4652c177b4a6201ec70834575
Eimen Hamedat <64800243+eimenhmdt@users.noreply.github.com>
2024-02-02 20:38:55 +0100

    Update route.ts

--- web/app/api/auth/callback/route.ts
+++ web/app/api/auth/callback/route.ts
@@ -1,6 +1,6 @@
 import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
-import { RedirectType, redirect } from 'next/navigation';
+import { redirect } from 'next/navigation';
 
 export async function GET(request) {
 	const requestUrl = new URL(request.url);
@@ -11,5 +11,5 @@ export async function GET(request) {
 		await supabase.auth.exchangeCodeForSession(code);
 	}
 
-	redirect("https://isaaceditor.com/editor", RedirectType.replace);
+	redirect('/editor');
 }
