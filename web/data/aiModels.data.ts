1e8563b243543b35b1b19ab27887f95871e19ac0
Eimen Hamedat <64800243+eimenhmdt@users.noreply.github.com>
2024-02-08 20:55:39 +0100

    fix ai model name

--- web/data/aiModels.data.ts
+++ web/data/aiModels.data.ts
@@ -1,7 +1,7 @@
 export const AIModelLocalStorageKey = 'AIModelSelection';
 
 export enum AIModels {
-	GPT_3_5 = 'gpt-3.5-turbo-0125’)',
+	GPT_3_5 = 'gpt-3.5-turbo-0125',
 	GPT_4 = 'gpt-4-0125-preview',
 }
 
